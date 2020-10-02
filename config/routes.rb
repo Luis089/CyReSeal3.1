e208559912d4021a3e56ee86fd4f1fa94ee1b817
Luis089 <sirluis50@googlemail.com>
2020-10-02 12:09:47 +0200

    deleted auto-generated migrations

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
   get "auditoren", to: "users#index"
   resources :users
+  resources :quizzes
+  resources :questions 
+  resources :answers 
+  resources :results 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20201002094053_create_results.rb
+++
@@ -1,8 +0,0 @@
-class CreateResults < ActiveRecord::Migration[6.0]
-  def change
-    create_table :results do |t|
-
-      t.timestamps
-    end
-  end
-end
