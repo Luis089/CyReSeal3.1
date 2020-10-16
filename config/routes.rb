2d97891aad8f5a95f768d3a05ec18d74e0d960a8
Luis089 <sirluis50@googlemail.com>
2020-10-16 11:13:26 +0200

    migrating views from CyreSeal4.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
+    resources :roles
+    resources :quizzes
+    resources :questions 
+    resources :answers 
+    resources :results 
     post "users/sign_up", to: "devise/registrations#create"
   end
   root "pages#landing"
   get "welcome", to: "pages#welcome"
   get "about", to: "pages#about"
   get "auditoren", to: "users#index"
-  resources :users
-  resources :quizzes
-  resources :questions 
-  resources :answers 
-  resources :results 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
