f0926eddc36cd7246ddbfc2cb8bb437b6089cb6b
Luis089 <sirluis50@googlemail.com>
2020-10-16 11:48:47 +0200

    asd

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    post "users/sign_up", to: "devise/registrations#create"
+  end
+  devise_for :users
   resources :roles
   resources :quizzes
   resources :questions 
   resources :answers 
   resources :results 
-  devise_for :users
-  devise_scope :user do
-    post "users/sign_up", to: "devise/registrations#create"
-  end
   root "pages#landing"
   get "welcome", to: "pages#welcome"
   get "about", to: "pages#about"
   get "auditoren", to: "users#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
